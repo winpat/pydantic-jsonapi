9ff7cdec74656fdd7be638e4ec087620727f98f6
Patrick Winter <patrickwinter@posteo.ch>
2020-10-07 16:38:07 +0200

    Fix typo in test case name

--- tests/test_resource_links.py
+++ tests/test_resource_links.py
@@ -9,16 +9,14 @@ class ThingWithLinks(BaseModel):
 
 
 class TestResourceLinks:
-    def test_follows_strucutre(self):
+    def test_follows_structure(self):
         structure_to_validate = {
-            'links': {
-                'self': '/person/walter',
-                'related': {
-                    'href': '/person/wendy',
-                    'meta': {
-                        'relationship': 'friend',
-                    },
-                }
+            "links": {
+                "self": "/person/walter",
+                "related": {
+                    "href": "/person/wendy",
+                    "meta": {"relationship": "friend",},
+                },
             }
         }
         validated = ThingWithLinks(**structure_to_validate)
@@ -26,33 +24,46 @@ class TestResourceLinks:
 
     def test_must_be_valid_map(self):
         with raises(ValidationError) as e:
-            ThingWithLinks(links=['walter', 'wendy'])
+            ThingWithLinks(links=["walter", "wendy"])
         assert e.value.errors() == [
-            {'loc': ('links',), 'msg': 'value is not a valid dict', 'type': 'type_error.dict'}
+            {
+                "loc": ("links",),
+                "msg": "value is not a valid dict",
+                "type": "type_error.dict",
+            }
         ]
 
     def test_values_must_be_a_str_or_link_href_object(self):
-        ThingWithLinks(links={'walter': ''})
-        ThingWithLinks(links={
-            'wendy': {
-                'href': '/person/wendy',
-                'meta': {
-                    'relationship': 'friend',
-                },
+        ThingWithLinks(links={"walter": ""})
+        ThingWithLinks(
+            links={
+                "wendy": {"href": "/person/wendy", "meta": {"relationship": "friend",},}
             }
-        })
-        invalid_string_error = {'loc': ('links', 'walter'), 'msg': 'str type expected', 'type': 'type_error.str'}
+        )
+        invalid_string_error = {
+            "loc": ("links", "walter"),
+            "msg": "str type expected",
+            "type": "type_error.str",
+        }
 
         with raises(ValidationError) as e:
-            ThingWithLinks(links={'walter': object()})
+            ThingWithLinks(links={"walter": object()})
         assert e.value.errors() == [
             invalid_string_error,
-            {'loc': ('links', 'walter'), 'msg': 'value is not a valid dict', 'type': 'type_error.dict'}
+            {
+                "loc": ("links", "walter"),
+                "msg": "value is not a valid dict",
+                "type": "type_error.dict",
+            },
         ]
 
         with raises(ValidationError) as e:
-            ThingWithLinks(links={'walter': {'href': '/people/123'}})
+            ThingWithLinks(links={"walter": {"href": "/people/123"}})
         assert e.value.errors() == [
             invalid_string_error,
-            {'loc': ('links', 'walter', 'meta'), 'msg': 'field required', 'type': 'value_error.missing'},
+            {
+                "loc": ("links", "walter", "meta"),
+                "msg": "field required",
+                "type": "value_error.missing",
+            },
         ]
